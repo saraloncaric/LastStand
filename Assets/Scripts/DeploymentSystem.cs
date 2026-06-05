8b35de53f7b6ab14b75530a7c0cfb07f36d03c32
saraloncaric <167783165+saraloncaric@users.noreply.github.com>
2026-06-05 21:14:06 +0200

    Meni dio za pozicije vojnika

--- Assets/Scripts/DeploymentSystem.cs
+++ Assets/Scripts/DeploymentSystem.cs
@@ -28,45 +28,83 @@ public class DeploymentSystem : MonoBehaviour
     int[] vojnikLokacija = new int[12];
 
     void Start() {
-        for (int i = 0; i < 12; i++) {
+        for (int i = 0; i < 12; i++)
             vojnikLokacija[i] = -1;
-        }
+
         oruzjePanel.SetActive(false);
-        PrikaziLokacije();
+        PrikaziVojnike();
     }
 
-    public void PrikaziLokacije() {
+    public void PrikaziVojnike() {
         foreach (Transform child in gumbContainer)
             Destroy(child.gameObject);
 
-        for (int i = 0; i < lokacije.Length; i++) {
+        for (int i = 0; i < 12; i++) {
             int index = i;
-            GameObject gumb = Instantiate(gumbPrefab, gumbContainer);
-            string status = lokacije[i].zauzeto ? " (zauzeto)" : " (slobodno)";
-            gumb.GetComponentInChildren<TextMeshProUGUI>().text = lokacije[i].naziv + status;
-            gumb.GetComponent<Button>().onClick.AddListener(() => PostaviVojnika(index));
+            GameObject red = Instantiate(gumbPrefab, gumbContainer);
+            
+            string status = vojnikPostavljen[i] ? " postavljen" : "";
+            red.transform.Find("Vojnik tekst").GetComponent<TextMeshProUGUI>().text = "Vojnik " + (i + 1) + status;
+            
+            red.transform.Find("Pozicija").GetComponent<Button>().onClick.AddListener(() => OtvoriLokacije(index));
+            red.transform.Find("Oruzje").GetComponent<Button>().onClick.AddListener(() => OtvoriOruzje(index));
         }
     }
 
-    public void PostaviVojnika(int index) {
-        if (lokacije[index].zauzeto) {
+    public void OtvoriLokacije(int vojnikIndex) {
+        odabraniVojnikIndex = vojnikIndex;
+
+        foreach (Transform child in gumbContainer)
+            Destroy(child.gameObject);
+
+        GameObject prazanGumb = Instantiate(oruzjeGumbPrefab, gumbContainer);
+        prazanGumb.GetComponentInChildren<TextMeshProUGUI>().text = "Makni vojnika";
+        prazanGumb.GetComponentInChildren<Button>().onClick.AddListener(() => MakniVojnika(vojnikIndex));
+
+        for (int i = 0; i < lokacije.Length; i++) {
+            int lokIndex = i;
+            if (lokacije[i].zauzeto) continue;
+
+            GameObject gumb = Instantiate(oruzjeGumbPrefab, gumbContainer);
+            gumb.GetComponentInChildren<TextMeshProUGUI>().text = lokacije[i].naziv;
+            gumb.GetComponentInChildren<Button>().onClick.AddListener(() => PostaviVojnika(lokIndex));
+        }
+    }
+
+    public void PostaviVojnika(int lokacijaIndex) {
+        if (lokacije[lokacijaIndex].zauzeto) {
             Debug.Log("Lokacija je zauzeta!");
             return;
         }
 
-        int cijena = vojnikPostavljen[odabraniVojnikIndex == -1 ? 0 : odabraniVojnikIndex] ? 2 : 1;
+        int vojnik = odabraniVojnikIndex == -1 ? 0 : odabraniVojnikIndex;
+        int cijena = vojnikPostavljen[vojnik] ? 2 : 1;
 
         if (economy.coins < cijena) {
             Debug.Log("Nema dovoljno coinsa!");
             return;
         }
 
+        int staraLokacija = vojnikLokacija[vojnik];
+        if (staraLokacija != -1) {
+            lokacije[staraLokacija].zauzeto = false;
+        }
+
         economy.coins -= cijena;
-        Instantiate(vojnikPrefab, lokacije[index].spawnPoint.position, lokacije[index].spawnPoint.rotation);
-        lokacije[index].zauzeto = true;
-        vojnikPostavljen[index] = true;
-        vojnikLokacija[index] = index;
-        PrikaziLokacije();
+        Instantiate(vojnikPrefab, lokacije[lokacijaIndex].spawnPoint.position, lokacije[lokacijaIndex].spawnPoint.rotation);
+        lokacije[lokacijaIndex].zauzeto = true;
+        vojnikPostavljen[vojnik] = true;
+        vojnikLokacija[vojnik] = lokacijaIndex;
+        PrikaziVojnike();
+    }
+    public void MakniVojnika(int vojnikIndex) {
+        int staraLokacija = vojnikLokacija[vojnikIndex];
+        if (staraLokacija != -1) {
+            lokacije[staraLokacija].zauzeto = false;
+            vojnikLokacija[vojnikIndex] = -1;
+            vojnikPostavljen[vojnikIndex] = false;
+        }
+        PrikaziVojnike();
     }
 
     public void OtvoriOruzje(int vojnikIndex) {
@@ -78,12 +116,10 @@ public class DeploymentSystem : MonoBehaviour
 
         int val = gameManager.trenutniVal;
 
-        if (val <= 1) {
+        if (val <= 1)
             DodajOruzje("Luk", 3);
-        }
-        else if (val == 2) {
+        else if (val == 2)
             DodajOruzje("Bodež", 5);
-        }
         else {
             DodajOruzje("Mač", 6);
             DodajOruzje("Sjekira", 7);
