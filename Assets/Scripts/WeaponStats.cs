e26d3ee5bd943b6a4d8c0dc1e38773c0ec559721
nmiljanic <nmiljanic@unipu.hr>
2026-06-06 14:24:52 +0200

    BETA WeaponStats.cs

--- Assets/Scripts/WeaponStats.cs
+++ Assets/Scripts/WeaponStats.cs
@@ -2,7 +2,54 @@ using UnityEngine;
 
 public class WeaponStats : MonoBehaviour
 {
-    public float damage = 20f;
-    public float projectileSpeed = 15f;
-    public float fireRate = 1.5f;
+    [Header("Val 1 - Luk i strijela")]
+    public float w1_damage = 15f;
+    public float w1_projectileSpeed = 20f;
+    public float w1_fireRate = 1.5f;
+    public GameObject w1_projectilePrefab;
+
+    [Header("Val 3 - Mač / Sjekira")]
+    public float w3_damage = 40f;
+    public float w3_fireRate = 1.0f;
+    public float w3_meleeRange = 2.5f;
+
+    [Header("Dijeli se između valova")]
+    public GameObject bloodCirclePrefab;
+
+    
+    [HideInInspector] public float damage;
+    [HideInInspector] public float projectileSpeed;
+    [HideInInspector] public float fireRate;
+    [HideInInspector] public float meleeRange;
+    [HideInInspector] public bool isMelee;
+    [HideInInspector] public GameObject projectilePrefab;
+
+    void Awake()
+    {
+        SetWave(1); 
+    }
+
+    public void SetWave(int wave)
+    {
+        switch (wave)
+        {
+            case 1:
+                damage = w1_damage;
+                projectileSpeed = w1_projectileSpeed;
+                fireRate = w1_fireRate;
+                projectilePrefab = w1_projectilePrefab;
+                isMelee = false;
+                break;
+
+            case 3:
+                damage = w3_damage;
+                fireRate = w3_fireRate;
+                meleeRange = w3_meleeRange;
+                projectilePrefab = null;
+                isMelee = true;
+                break;
+        }
+
+        Debug.Log($"[WeaponStats] Val {wave}: isMelee={isMelee}, damage={damage}");
+    }
 }
